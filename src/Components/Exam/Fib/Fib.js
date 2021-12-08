26bf76f54aef9a579bc11a5ed962f8fb2f45c924
Nachiket <nachikotalwar@gmail.com>
2021-12-08 21:49:37 +0530

    setup1

---
+++ src/Components/Exam/Fib/Fib.js
@@ -0,0 +1,11 @@
+import './Fib.css';
+
+const Fib = () => {
+    return (
+        <div className="Fib">
+        <h1>Fib</h1>
+        </div>
+    );
+}
+    
+export default Fib;
---
+++ src/Containers/Recruitment.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import Exam from "../Components/Exam/Exam";
+
+const Recruitment = () => {
+  const [show, setShow] = useState(false);
+
+  const showdataArray = () => {
+    setShow(!show);
+};
+    
+  return (
+    <div>
+      {show ? <Exam /> : 
+      <div className="flex flex-col items-center">
+      <div className=" flex justify-center items-center">
+        <div className=" bg-red-900 h-64 w-56 mt-24 flex flex-col justify-around items-center">
+          <div className="bg-red-600 h-24 w-24 rounded-full flex items-center justify-center">
+            <h3 className=" text-6xl">S</h3>
+          </div>
+          <h3>Satvik Sinha</h3>
+        </div>
+      </div>
+      <button
+        className="bg-red-700 relative top-12 rounded-md p-3 mb-12"
+        onClick={showdataArray}
+      >
+        Start test
+      </button>
+      
+    </div>
+      }
+    </div>
+  );
+};
+
+export default Recruitment;
