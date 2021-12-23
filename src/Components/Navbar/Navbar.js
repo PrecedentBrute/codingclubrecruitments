ff50e808f08a03bf54134a1d7f8104386aba05d2
Harsh Neema <F20200878@pilani.bits-pilani.ac.in>
2021-12-23 22:18:34 +0530

    change

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = ({ toggle }) => {
   return (
     <nav
-      className='flex justify-between items-center h-16 bg-ccblack text-white relative shadow-sm font-mono'
+      className='flex justify-between items-center h-16 bg-black text-white relative shadow-sm font-mono'
       role='navigation'
     >
       <Link to='/' className='pl-8'>
