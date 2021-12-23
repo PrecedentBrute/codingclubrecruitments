d40757ecc92624ff1c27e5db9a959238b1857d5c
Harsh Neema <F20200878@pilani.bits-pilani.ac.in>
2021-12-23 23:47:35 +0530

    change 2

--- src/Containers/Recruitment/Recruitment.js
+++ src/Containers/Recruitment/Recruitment.js
@@ -17,15 +17,15 @@ const Recruitment = () => {
       {show ? <Exam /> : 
       <div className="flex flex-col items-center">
       <div className=" flex justify-center items-center">
-        <div className=" bg-transparent h-64 w-56 mt-24 flex flex-col justify-around items-center">
-          <div className="bg-red-600 h-24 w-24 rounded-full flex items-center justify-center">
-            <h3 className=" text-6xl">S</h3>
+        <div className=" h-64 w-56 mt-24 flex flex-col justify-around items-center recruitment-card">
+          <div className="bg-red-600 h-24 w-24 rounded-full flex items-center justify-center recruitment-card-image" >
+            
           </div>
-          <h3>Satvik Sinha</h3>
+          <h3 className="text-white">Please Log In Satvik</h3>
         </div>
       </div>
       <button
-        className="bg-red-700 relative top-12 rounded-md p-3 mb-12"
+        className="p-10 mt-10 py-2 rounded-full bg-red-500 text-gray-100  focus:outline-none"
         onClick={showdataArray}
       >
         Start test
