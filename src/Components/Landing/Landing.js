d7de6fec20aace3b52e4baede63bb13e867650be
unknown <anantjain.8k@gmail.com>
2022-01-05 19:05:38 +0530

    Y scrolling stopped

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -35,7 +35,7 @@ const Landing = () => {
     <div class="page-bg"></div>
 
     <div class="animation-wrapper">
-      <div class="particle particle-1"></div>
+      <div class="particle particle-1"></div> 
       <div class="particle particle-2"></div>
       <div class="particle particle-3"></div>
       <div class="particle particle-4"></div>
